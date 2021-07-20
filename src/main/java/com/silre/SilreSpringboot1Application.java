a359dd1e93a24cd09b41908f1a2a66db131587ab
andyzhongzxy <z6262690@qq.com>
2021-07-20 12:31:13 +0800

    first pull

---
+++ src/main/java/com/silre/SilreSpringboot1Application.java
@@ -0,0 +1,13 @@
+package com.silre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilreSpringboot1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SilreSpringboot1Application.class, args);
+    }
+
+}
