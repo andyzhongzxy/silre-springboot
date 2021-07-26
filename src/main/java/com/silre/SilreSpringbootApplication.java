f3025a55be6e465057de21bc1878fc16895f7dd0
andyzhongzxy <z6262690@qq.com>
2021-07-26 22:19:08 +0800

    add thymeleaf

--- src/main/java/com/silre/SilreSpringbootApplication.java
+++ src/main/java/com/silre/SilreSpringbootApplication.java
@@ -2,6 +2,9 @@ package com.silre;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class SilreSpringbootApplication {
--- src/main/java/com/silre/controller.java
+++ src/main/java/com/silre/controller.java
@@ -1,23 +1,15 @@
 package com.silre;
 
-import com.silre.config.school;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-/**
- * @Author: andyzhongzxy
- * @Date: create in 2021/7/20 13:21
- */
+
 
 @Controller
 public class controller {
-
-    @Autowired
-    private school school;
-    @RequestMapping(value = "/hello")
-    public @ResponseBody String say(){
-        return school.getName();
+    @RequestMapping("/test")
+    public String index(){
+        return "test";
     }
 }
